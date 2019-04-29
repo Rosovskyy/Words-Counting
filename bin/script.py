e5f50412583da94d7f75d5ef82b2283e403e840a
Serhii Rosovskyi <30779010+Rosovskyy@users.noreply.github.com>
2019-04-29 13:14:51 +0300

    script fixed

--- bin/script.py
+++ bin/script.py
@@ -27,7 +27,7 @@ def run_multi_thread_solution(times, multi_thread, num_of_threads, input_file, o
     min_multi_thread = [0, 0, sys.maxsize]
     data = run_one_time(one_thread, input_file)
 
-    for i in range(0, times):
+    for _ in range(0, times):
         res_multi = parse_result(subprocess.Popen([multi_thread, num_of_threads, input_file],
                                                   stdout=subprocess.PIPE).communicate()[0].decode('utf-8'))
 
@@ -39,27 +39,25 @@ def run_multi_thread_solution(times, multi_thread, num_of_threads, input_file, o
         if res_multi[2] < min_multi_thread[2]:
             min_multi_thread = res_multi
 
-    for i in min_multi_thread:
-        print("[" + str(i) + "]")
+    text = ["Reading time: ", "Calculating time: ", "Total time: "]
+    for i, j in enumerate(min_multi_thread):
+        print("[" + text[i] + str(j) + "]")
     if results_are_same_1:
         print("Results are the same for all the multiple solutions runs")
     else:
         print("Results are NOT the same for all the multiple solutions runs")
 
-    return result
+    return data
 
 
 def run_one_thread_solution(times, one_thread, input_file):
     min_one_thread = [0, 0, sys.maxsize]
     results_are_same_2 = True
-    result = 0
     data = run_one_time(one_thread, input_file)
 
-    for i in range(0, times):
+    for _ in range(0, times):
         res_one = parse_result(
             subprocess.Popen([one_thread, input_file], stdout=subprocess.PIPE).communicate()[0].decode('utf-8'))
-
-        print(res_one)
 
         if not check_results(data):
             results_are_same_2 = False
@@ -67,32 +65,37 @@ def run_one_thread_solution(times, one_thread, input_file):
         if res_one[2] < min_one_thread[2]:
             min_one_thread = res_one
 
-    for i in min_one_thread:
-        print("[" + str(i) + "]")
+    text = ["Reading time: ", "Calculating time: ", "Total time: "]
+    for i, j in enumerate(min_one_thread):
+        print("[" + text[i] + str(j) + "]")
     if results_are_same_2:
         print("Results are the same for all the one solutions runs")
     else:
         print("Results are NOT the same for all the one solutions runs")
 
-    return result
+    return data
 
 
 def main():
-    if len(sys.argv) != 5:
-        print('Usage: <program_name> <number_of_threads_for_multiple_threads_program>'
+    if len(sys.argv) != 5 or len(sys.argv) != 3:
+        print('Usage for multiple_thread: <program_name> <number_of_threads>'
               ' <times_to_run_the_program> <multiple/both> <input_file>')
+        print('\nUsage for one_thread: <program_name> <times_to_run_the_program> <input_file>')
         exit(-1)
 
     one_thread = './one_thread_solution'
     multi_thread = './multi_thread_solution'
 
+    if len(sys.argv == 3):
+        times = sys.argv[1]
+        input_file = sys.argv[2]
+        run_one_thread_solution(times, one_thread, input_file)
+        exit(-1)
+
     num_of_threads = sys.argv[1]
     times = int(sys.argv[2])
     threads_to_run = sys.argv[3]
     input_file = sys.argv[4]
-
-    run_multi_thread_solution(times, multi_thread, num_of_threads, input_file, one_thread)
-    exit(-1)
 
     if threads_to_run == 'multiple':
         run_multi_thread_solution(times, multi_thread, num_of_threads, input_file, one_thread)
